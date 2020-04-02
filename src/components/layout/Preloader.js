da49b4c0be13b7deaf6d04263054160a3b3591a0
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-02 12:54:35 +0530

    initial setup

---
+++ src/components/layout/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = props => {
+  return (
+    <div className="progress blue lighten-4">
+      <div className="inderterminate blue"></div>
+    </div>
+  );
+};
+
+export default Preloader;
